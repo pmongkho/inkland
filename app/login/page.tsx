131ec31df2af0d72135b55d1eeead7ee414c0964
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-08-09 02:25:04 -0500

    fourth commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,6 +6,10 @@ import React from 'react'
 import { authOptions } from '../api/auth/[...nextauth]/options'
 
 export default async function SignIn() {
+	const session = await getServerSession(authOptions)
+	if (session?.user) {
+		redirect('/')
+	}
 
 	return (
 		<div className='h-screen'>
